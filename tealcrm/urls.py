096c2855137f8101906a40bdb183ec7cba20ed82
gasanovazer <gasanovazer@outlook.com>
2024-07-09 23:46:27 +0400

    Implement solution for teams
    Creat teams app
    Model for teams
    When yo create a lead ande a client, make sure team is set
    if you're not a member of a team, creat one
    if you're the owner, make it possible to edit team

--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -3,12 +3,14 @@ from django.contrib.auth import views
 from django.urls import path, include
 
 from core.views import index, about
-from userprofile.views import singup
+from userprofile.views import myaccount, singup
 
 urlpatterns = [
     path('', index, name='index'),
     path("dashboard/leads/", include('lead.urls')),
     path("dashboard/clients/", include('client.urls')),
+    path('dashboard/myaccount/', myaccount, name='myaccount'),
+    path("dashboard/teams/", include('team.urls')),
     path("dashboard/", include('dashboard.urls')),
     path('about/', about, name='about'),
     path('sing-up/', singup, name='singup'),
