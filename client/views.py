32088634b17be568c2fe4bfca42e8e119d9b4084
gasanovazer <gasanovazer@outlook.com>
2024-07-09 15:08:13 +0400

    Change and delete clients

--- client/views.py
+++ client/views.py
@@ -40,3 +40,31 @@ def clients_add(request):
     return render(request, 'client/clients_add.html', {
         'form': form
     })
+
+@login_required
+def clients_edit(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddClientForm(request.POST, instance=client)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "The changes was saved.")
+
+            return redirect('clients_list')
+    else:
+        form = AddClientForm(instance=client)
+    
+    return render(request, "client/clients_edit.html", {
+        'form': form
+    })
+
+@login_required
+def clients_delete(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    client.delete()
+
+    messages.success(request, "The client was deleted.")
+
+    return redirect('clients_list')
