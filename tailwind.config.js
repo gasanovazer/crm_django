967662e7ffcaafd26226e32dacdb0a92d83b17c7
gasanovazer <gasanovazer@outlook.com>
2024-07-11 09:47:38 +0400

    Refactoring. Install Tailwind instead of using the CDN

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'client/tamplates/client/*.html',
+    'core/tamplates/core/*.html',
+    'dashboard/tamplates/dashboard/*.html',
+    'lead/tamplates/lead/*.html',
+    'team/tamplates/team/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
